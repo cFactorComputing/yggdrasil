b291591a856b1a30835691e48a236e86c067ca93
Anoop K John <anoop.kjohn@swiftwallet.io>
2017-04-05 16:54:46 +0530

    Email adapter for OTP & account transfer

--- yggdrasil-adapters/e-mail-adapter/src/main/java/io/swiftwallet/yggdrasil/adapters/email/account/processors/AccountTransferEmailTemplateProcessor.java
+++ yggdrasil-adapters/e-mail-adapter/src/main/java/io/swiftwallet/yggdrasil/adapters/email/account/processors/AccountTransferEmailTemplateProcessor.java
@@ -12,23 +12,30 @@ import java.util.Date;
 
 @Component
 public class AccountTransferEmailTemplateProcessor extends AbstractEmailTemplateProcessor {
+    private static final String VALUE = "value";
+    private static final String DATE = "date";
+    private static final String DATE_FORMAT = "dateFormat";
+    private static final String WALLET_USER = "walletUser";
+    private static final String DESCRIPTION = "description";
+    private static final String TRANSACTION_ID = "transactionId";
+    private static final String CURRENCY_SYMBOL = "currencySymbol";
 
     @Override
     protected void buildEmailContextParams(final Exchange exchange, final Context context) {
         final EmailRequest emailRequest = exchange.getIn().getBody(EmailRequest.class);
-        final Double amount = emailRequest.getProperty("value");
+        final Double amount = emailRequest.getProperty(VALUE);
         final BigDecimal value = BigDecimal.valueOf(amount);
-        final Date date = new Date(emailRequest.<Long>getProperty("date"));
-        final String transactionId = emailRequest.getProperty("transactionId");
+        final Date date = new Date(emailRequest.<Long>getProperty(DATE));
+        final String transactionId = emailRequest.getProperty(TRANSACTION_ID);
 
-        final String currencySymbol = emailRequest.getProperty("currencySymbol");
-        final String dateFormat = emailRequest.getProperty("dateFormat");
-        context.setVariable("value", value);
-        context.setVariable("walletUser", emailRequest.getProperty("walletUser"));
-        context.setVariable("description", emailRequest.getProperty("description"));
-        context.setVariable("date", date);
-        context.setVariable("transactionId", transactionId);
-        context.setVariable("currencySymbol", StringUtils.defaultIfEmpty(currencySymbol, "INR"));
-        context.setVariable("dateFormat", StringUtils.defaultIfEmpty(dateFormat, "dd-MMM-yyyy"));
+        final String currencySymbol = emailRequest.getProperty(CURRENCY_SYMBOL);
+        final String dateFormat = emailRequest.getProperty(DATE_FORMAT);
+        context.setVariable(VALUE, value);
+        context.setVariable(WALLET_USER, emailRequest.getProperty(WALLET_USER));
+        context.setVariable(DESCRIPTION, emailRequest.getProperty(DESCRIPTION));
+        context.setVariable(DATE, date);
+        context.setVariable(TRANSACTION_ID, transactionId);
+        context.setVariable(CURRENCY_SYMBOL, StringUtils.defaultIfEmpty(currencySymbol, "INR"));
+        context.setVariable(DATE_FORMAT, StringUtils.defaultIfEmpty(dateFormat, "dd-MMM-yyyy"));
     }
 }
