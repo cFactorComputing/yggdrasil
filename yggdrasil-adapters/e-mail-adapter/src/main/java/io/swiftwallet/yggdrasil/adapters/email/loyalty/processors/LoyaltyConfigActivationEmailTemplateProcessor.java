b78705d3e03f2c3dfc581e06cbe2306b360bf784
Aby Jose Abraham <aby.joseabraham@swiftwallet.io>
2017-06-01 17:08:03 +0530

    feature/SW-160: Configure expiry term for loyalty program

--- yggdrasil-adapters/e-mail-adapter/src/main/java/io/swiftwallet/yggdrasil/adapters/email/loyalty/processors/LoyaltyConfigActivationEmailTemplateProcessor.java
+++ yggdrasil-adapters/e-mail-adapter/src/main/java/io/swiftwallet/yggdrasil/adapters/email/loyalty/processors/LoyaltyConfigActivationEmailTemplateProcessor.java
@@ -13,19 +13,15 @@ import java.util.Date;
 public class LoyaltyConfigActivationEmailTemplateProcessor extends AbstractEmailTemplateProcessor {
     private static final String TITLE = "title";
     private static final String ACTIVATED = "activated";
-    private static final String EXPIRY_DATE = "expiryDate";
-    private static final String DATE_FORMAT = "dateFormat";
+    private static final String EXPIRY_TERM = "expiryTerm";
 
     @Override
     protected void buildEmailContextParams(final Exchange exchange, final Context context) {
         final EmailRequest emailRequest = exchange.getIn().getBody(EmailRequest.class);
         final String title = emailRequest.getProperty(TITLE);
-        final Date expiryDate = new Date(emailRequest.<Long>getProperty(EXPIRY_DATE));
-        final String dateFormat = emailRequest.getProperty(DATE_FORMAT);
 
         context.setVariable(TITLE, title);
-        context.setVariable(EXPIRY_DATE, expiryDate);
+        context.setVariable(EXPIRY_TERM, emailRequest.getProperty(EXPIRY_TERM));
         context.setVariable(ACTIVATED, emailRequest.getProperty(ACTIVATED));
-        context.setVariable(DATE_FORMAT, StringUtils.defaultIfEmpty(dateFormat, "dd-MMM-yyyy"));
     }
 }
